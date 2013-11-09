cd2c7e8b50cfc4f587eb85a369edfb484cdcf142
Fei <fei.fei2liu@gmail.com>
2013-11-09 16:31:12 -0500

    Updates from Parse

--- chrome_extension/browseraction/popup.js
+++ chrome_extension/browseraction/popup.js
@@ -18,6 +18,38 @@ $(function() {
     $('#button').click(function(){
         sendClicks();
     });
+    
+    var objectIDarray = ["jCUiHfDH8Y", "3xO9WcjatS", "oSgbevAvgB"];
+    
+    Parse.initialize("WbXWyCHDLZJbApq74W1a95YkYekli5wDMhBGVYCj", "4iwEiCRAR2ynXQ6V4aqC5YdKkwkpvRUITUjgBkfR");
+    var billingAccount = Parse.Object.extend("billingAccount");
+	var query = new Parse.Query(billingAccount);
+	var name, company, photo, article_header, article_link, political_leaning, politics, lifestyle_title, lifestyle, whereabouts;
+
+	for (i = 0; i < objectIDarray.length; i++) {
+		query.get(objectIDarray[i], {
+		  success: function(billingAccount) {
+		  	name = billingAccount.get("name");
+		  	company = billingAccount.get("company");
+		  	photo = billingAccount.get("photo");
+		  	article_header = billingAccount.get("article_header");
+		  	article_link = billingAccount.get("article_link");
+		  	political_leaning = billingAccount.get("political_leaning");
+		  	politics = billingAccount.get("politics");
+		  	lifestyle_title = billingAccount.get("lifestyle_title");
+		  	lifestyle = billingAccount.get("lifestyle");
+		  	whereabouts = billingAccount.get("whereabouts");
+
+		  	$('#tracking').append('<h1>' + name + '</h1>' + company);
+		  },
+		  error: function(object, error) {
+		    console.log("not found");
+		    // The object was not retrieved successfully.
+		    // error is a Parse.Error with an error code and description.
+		  }
+		});
+	}
+
 });
 
 /*
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -5,11 +5,11 @@ chrome.runtime.onMessage.addListener(
                 "from the extension");
 
         if (request["greeting"]) {
+
         	console.log("Value: " + request["greeting"]);
 			$(document).mousemove(function(e) {
 			    window.x = e.pageX;
 			    window.y = e.pageY;
-			    console.log(window.x);
 			    $("body").append('<div id="eric"></div>');
                	$('#eric').css({"width":"105px", "height":"105px", "background":"url('http://trytobegood.com/hack/eric_cursor.png') no-repeat", "top": window.y-25, "left": window.x+25, "z-index":"10000000", "position":"absolute"});
                	
